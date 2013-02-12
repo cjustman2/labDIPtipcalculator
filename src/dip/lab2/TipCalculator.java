6cf6a9aeb072041880598722e596174ff1cf438b
cjustman2@my.wctc.edu <>
2013-02-12 13:56:29 -0600

    commit2

---
+++ src/dip/lab2/TipCalculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2;
+
+
+
+
+
+/**
+ *
+ * @author chris
+ */
+public interface TipCalculator {
+    
+    public abstract double getTip();
+    
+  
+    
+    
+    
+    
+}
